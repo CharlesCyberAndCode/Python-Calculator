0bd6ab0dbfd2aa77d07e4460b5206ec358ee00dc
CharlesCodes <136041610+CharlesCyberAndCode@users.noreply.github.com>
2024-09-28 12:59:03 -0400

    Add files via upload
    Added backspace functionality. History is working properly, cleaned up some inputs. Will be adding the previous numbers and functions used to get final calculations to history as well. Will also be making Tinker GUI in future.

--- OverEngineeredCalculator.py
+++ OverEngineeredCalculator.py
@@ -6,63 +6,82 @@ while True:
     historyList = []
     while True:
         nums = []       
-        #FIX THIS LATER OR YOURE A DICKHEAD
+        #FIX THIS LATER OR YOURE A weenie
         #try:
        
-        
-        lengthOfCalc = int(input("How many numbers will you be using? "))
-        #except ValueError:
-        ### print("Must use numbers to enter value.")
-        
-
-        # This adds the user input into the list for use later.
-        for i in range (lengthOfCalc):
-            nums.append(float(input("Choose Number: "))) 
-            print(nums[0:])
-
-        #Declaring after setting up nums list
-        finalCalc = nums[0]
-
-
-
-        
-           
         while True:
-            math = input("Choose operator to use. Ex. +, /, *, -: ")
-            if math == "+":
-                for i in range(len(nums[1:])):
-                    finalCalc = finalCalc + nums[i+1]
-                print(finalCalc)
-                historyList.append(int(finalCalc))
-                break
-            elif math == "/":  
-                for i in range(len(nums[1:])):
-                    finalCalc = finalCalc / nums[i+1]
-                print(finalCalc)
-                historyList.append(int(finalCalc))
-                break
-            elif math == "*" or math == "x":  
-                for i in range(len(nums[1:])):
-                    finalCalc = finalCalc * nums[i+1]
-                print(finalCalc)
-                historyList.append(int(finalCalc))
-                break
-            elif math == "-":  
-                for i in range(len(nums[1:])):
-                    finalCalc = finalCalc - nums[i+1]
-                print(finalCalc)
-                historyList.append(int(finalCalc))
-                break
-            else:
-                print("Sorry, you did not enter an accepted operator. Try again.")
+            lengthOfCalc = int(input("How many numbers will you be using? "))
+            #except ValueError:
+            ### print("Must use numbers to enter value.")
+            
+            while True:
+                # This adds the user input into the list for use later.
+            
+                for i in range (lengthOfCalc):
+                    inputNum1 = 0
+                    print("Choose Number: ")
+                    print("To remove last value, enter: 0")
+                    inputNum1 = float(input("")) 
                 
-        print("Would you like to see a history of previous calculations?")
-        print("1. Yes")
-        print("2. No")
-        print("Choose a number.")
-        history = input("")
-        if history == "1":
-            print(historyList[0:])
+                    ## Backspace functionality    
+                    if int(inputNum1) == 0:
+                            nums.pop()
+                            print(nums)
+                            
+                            break
+                    else:
+                        nums.append(float(inputNum1))
+                        print(nums)
+                        continue
+                
+
+                #Declaring after setting up nums list
+                finalCalc = nums[0]
+
+
+
+                
+                
+                while True:
+                    math = input("Choose operator to use. Ex. +, /, *, -: ")
+                    if math == "+":
+                        for i in range(len(nums[1:])):
+                            finalCalc = finalCalc + nums[i+1]
+                        print(finalCalc)
+                        historyList.append(int(finalCalc))
+                        break
+                    elif math == "/":  
+                        for i in range(len(nums[1:])):
+                            finalCalc = finalCalc / nums[i+1]
+                        print(finalCalc)
+                        historyList.append(int(finalCalc))
+                        break
+                    elif math == "*" or math == "x":  
+                        for i in range(len(nums[1:])):
+                            finalCalc = finalCalc * nums[i+1]
+                        print(finalCalc)
+                        historyList.append(int(finalCalc))
+                        break
+                    elif math == "-":  
+                        for i in range(len(nums[1:])):
+                            finalCalc = finalCalc - nums[i+1]
+                        print(finalCalc)
+                        historyList.append(int(finalCalc))
+                        break
+                    else:
+                        print("Sorry, you did not enter an accepted operator. Try again.")
+                break
+                    
+            print("Would you like to see a history of previous calculations?")
+            print("1. Yes")
+            print("2. No")
+            print("Choose a number.")
+            history = input("")
+            if history == "1":
+                print(historyList[0:])
+                
+            break
+            
         ## Make a dictionary to retain the numbers and opperator used for calculation with the finalCalc?
 
 
