bb29f458bce157d626e85e67b2ca1d0796ae584d
CharlesCodes <136041610+CharlesCyberAndCode@users.noreply.github.com>
2024-09-29 18:16:07 -0400

    Add files via upload
    Cleaned up UX a bit. Removed necessity for user to enter how many numbers they would like to use, user can now enter as many as they need. User can choose to move on to choosing operator and finishing calculation at any point as well. Fixed bug in deletion functionality. User can now delete as many times in a row as they'd like, and also can now delete final number in their list. Cleaned up code and shortened. 
    Still need to implement a GUI, exception handling, and additional functionality to the history feature. History feature will show the numbers and operators that led to the answer as well as the answer.

--- OverEngineeredCalculator.py
+++ OverEngineeredCalculator.py
@@ -1,90 +1,76 @@
-while True:
-    ## Calculator V.1
+##### Calculator V.1
 
+## MAKE EXCEPTION HANDLING FOR EVERYTHING
+while True:
     # Setting up variables/list for use. Using list so user can add as many values as necessary
     
     historyList = []
     while True:
         nums = []       
-        #FIX THIS LATER OR YOURE A weenie
-        #try:
-       
+
+# Allows user to input as many numbers as they like, del/delete upper or lower will delete last number
+# Allows user to enter 0 to progress to doing calculation
         while True:
-            lengthOfCalc = int(input("How many numbers will you be using? "))
-            #except ValueError:
-            ### print("Must use numbers to enter value.")
+            inputNum1 = 1
+            while inputNum1 != 0:
+                print("Choose a number to input")
+                print("To delete last number type 'delete' or 'del'")
+                print("To continue to calculation type 0")
+                inputNum1 = (input("")) 
+                #"Backspace function"
+                if inputNum1.lower() == "del" or inputNum1.lower() == 'delete':
+                    nums.pop()
+                    print(nums)
+                # Allows user to move on to calculation.
+                elif (inputNum1 == "0"): 
+                    break
+                # Adds number entered into the 'nums' list for later use
+                elif True:
+                    nums.append(float(inputNum1))
+                    print(nums)
             
+            # Declaring after setting up nums list so no exceptions
+            finalCalc = nums[0] 
+            
+            # Calculation functionality seperated by opperator      
             while True:
-                # This adds the user input into the list for use later.
-            
-                for i in range (lengthOfCalc):
-                    inputNum1 = 0
-                    print("Choose Number: ")
-                    print("To remove last value, enter: 0")
-                    inputNum1 = float(input("")) 
-                
-                    ## Backspace functionality    
-                    if int(inputNum1) == 0:
-                            nums.pop()
-                            print(nums)
-                            
-                            break
-                    else:
-                        nums.append(float(inputNum1))
-                        print(nums)
-                        continue
-                
-
-                #Declaring after setting up nums list
-                finalCalc = nums[0]
-
-
-
-                
-                
-                while True:
-                    math = input("Choose operator to use. Ex. +, /, *, -: ")
-                    if math == "+":
-                        for i in range(len(nums[1:])):
-                            finalCalc = finalCalc + nums[i+1]
-                        print(finalCalc)
-                        historyList.append(int(finalCalc))
-                        break
-                    elif math == "/":  
-                        for i in range(len(nums[1:])):
-                            finalCalc = finalCalc / nums[i+1]
-                        print(finalCalc)
-                        historyList.append(int(finalCalc))
-                        break
-                    elif math == "*" or math == "x":  
-                        for i in range(len(nums[1:])):
-                            finalCalc = finalCalc * nums[i+1]
-                        print(finalCalc)
-                        historyList.append(int(finalCalc))
-                        break
-                    elif math == "-":  
-                        for i in range(len(nums[1:])):
-                            finalCalc = finalCalc - nums[i+1]
-                        print(finalCalc)
-                        historyList.append(int(finalCalc))
-                        break
-                    else:
-                        print("Sorry, you did not enter an accepted operator. Try again.")
-                break
-                    
-            print("Would you like to see a history of previous calculations?")
-            print("1. Yes")
-            print("2. No")
-            print("Choose a number.")
+                math = input("Choose operator to use. Ex. +, /, *, -: ")
+                if math == "+":
+                    for i in range(len(nums[1:])):
+                        finalCalc = finalCalc + nums[i+1]
+                    print("Answer: ", finalCalc)
+                    historyList.append(float(finalCalc))
+                    break
+                elif math == "/":  
+                    for i in range(len(nums[1:])):
+                        finalCalc = finalCalc / nums[i+1]
+                    print("Answer: ", finalCalc)
+                    historyList.append(float(finalCalc))
+                    break
+                elif math == "*" or math == "x":  
+                    for i in range(len(nums[1:])):
+                        finalCalc = finalCalc * nums[i+1]
+                    print("Answer: ", finalCalc)
+                    historyList.append(float(finalCalc))
+                    break
+                elif math == "-":  
+                    for i in range(len(nums[1:])):
+                        finalCalc = finalCalc - nums[i+1]
+                    print("Answer: ", finalCalc)
+                    historyList.append(float(finalCalc))
+                    break
+                else:
+                    print("Sorry, you did not enter an accepted operator. Try again.")
+                    break
+                        
+            print("Type 1 to see a history of previous calculations.")
+            print("Type anything else to continue.")
             history = input("")
             if history == "1":
-                print(historyList[0:])
-                
+                print(historyList[0:])            
             break
-            
-        ## Make a dictionary to retain the numbers and opperator used for calculation with the finalCalc?
-
-
+                
+            ## Make a dictionary to retain the numbers and opperator used for calculation with the finalCalc?
 
 
 
