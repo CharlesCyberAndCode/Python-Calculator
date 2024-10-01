9319f08b73788f3b53b6686457802848865b38dd
CharlesCodes <136041610+CharlesCyberAndCode@users.noreply.github.com>
2024-10-01 18:13:50 -0400

    Add files via upload
    Fixed bug in calculator. Cleaned up UX and made hitting enter take you to the calculation instead of hitting 0 to do that. That choice was a bit strange by me... Well fixed now! Will be adding the ability to square single numbers, as well as raise numbers to a power in future. Will also still be adding history showing the numbers that led to answers, and GUI.

---
+++ OverEngineeredCalculatorMAIN.py
@@ -0,0 +1,79 @@
+##### Calculator V.1
+
+## MAKE EXCEPTION HANDLING FOR EVERYTHING
+while True:
+    # Setting up variables/list for use. Using list so user can add as many values as necessary
+    
+    historyList = []
+    while True:
+        nums = []       
+
+# Allows user to input as many numbers as they like, del/delete upper or lower will delete last number
+# Allows user to enter 0 to progress to doing calculation
+        while True:
+            inputNum1 = 1
+            while inputNum1 != "":
+                print("Choose a number to input")
+                print("To delete last number type 'delete' or 'del'")
+                print("To continue to calculation hit enter")
+                inputNum1 = (input("")) 
+                #"Backspace function"
+                if inputNum1.lower() == "del" or inputNum1.lower() == 'delete':
+                    nums.pop()
+                    print(nums)
+                # Allows user to move on to calculation.
+                elif (inputNum1 == ""): 
+                    break
+                # Adds number entered into the 'nums' list for later use
+                elif True:
+                    nums.append(float(inputNum1))
+                    print(nums)
+            
+            # Declaring after setting up nums list so no exceptions
+            finalCalc = nums[0] 
+            
+            # Calculation functionality seperated by opperator      
+            while True:
+                math = input("Choose operator to use. Ex. +, /, *, -: ")
+                if math == "+":
+                    for i in range(len(nums[1:])):
+                        finalCalc = finalCalc + nums[i+1]
+                    print("Answer: ", finalCalc)
+                    historyList.append(float(finalCalc))
+                    break
+                elif math == "/":  
+                    for i in range(len(nums[1:])):
+                        finalCalc = finalCalc / nums[i+1]
+                    print("Answer: ", finalCalc)
+                    historyList.append(float(finalCalc))
+                    break
+                elif math == "*" or math == "x":  
+                    for i in range(len(nums[1:])):
+                        finalCalc = finalCalc * nums[i+1]
+                    print("Answer: ", finalCalc)
+                    historyList.append(float(finalCalc))
+                    break
+                elif math == "-":  
+                    for i in range(len(nums[1:])):
+                        finalCalc = finalCalc - nums[i+1]
+                    print("Answer: ", finalCalc)
+                    historyList.append(float(finalCalc))
+                    break
+                else:
+                    print("Sorry, you did not enter an accepted operator. Try again.")
+                    print(nums)                   
+               
+            # History functionality to see previous answers. Make show nums and operators used in future.            
+            print("Type 1 to see a history of previous calculations.")
+            print("Type anything else to continue.")
+            history = input("")
+            if history == "1":
+                print(historyList[0:])            
+            break
+                
+            ## Make a dictionary to retain the numbers and opperator used for calculation with the finalCalc?
+
+
+
+
+
