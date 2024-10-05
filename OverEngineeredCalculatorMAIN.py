8cb87fe0c2a009712fc6881809060588805fc918
CharlesCodes <136041610+CharlesCyberAndCode@users.noreply.github.com>
2024-10-04 20:07:14 -0400

    Update
    Added square root and exponentials support. Added disclaimer for current ability of those functions.

--- OverEngineeredCalculatorMAIN.py
+++ OverEngineeredCalculatorMAIN.py
@@ -1,7 +1,15 @@
 ##### Calculator V.1
 
 ## MAKE EXCEPTION HANDLING FOR EVERYTHING
+print("Overly Engineered Calculator v1.4")
+print("Square root can only affect single numbers, exponents also only affect single numbers")
+input("Accept: ")
+print("")
+print("")
+print("")
+
 while True:
+    import math
     # Setting up variables/list for use. Using list so user can add as many values as necessary
     
     historyList = []
@@ -34,28 +42,41 @@ while True:
             
             # Calculation functionality seperated by opperator      
             while True:
-                math = input("Choose operator to use. Ex. +, /, *, -: ")
-                if math == "+":
+                numIn = input("Choose operator to use. Ex. +, /, *, -, ^2, Square root: ")
+                if numIn == "+":
                     for i in range(len(nums[1:])):
                         finalCalc = finalCalc + nums[i+1]
                     print("Answer: ", finalCalc)
                     historyList.append(float(finalCalc))
                     break
-                elif math == "/":  
+                elif numIn == "/":  
                     for i in range(len(nums[1:])):
                         finalCalc = finalCalc / nums[i+1]
                     print("Answer: ", finalCalc)
                     historyList.append(float(finalCalc))
                     break
-                elif math == "*" or math == "x":  
+                elif numIn == "*" or numIn.lower() == "x":  
                     for i in range(len(nums[1:])):
                         finalCalc = finalCalc * nums[i+1]
                     print("Answer: ", finalCalc)
                     historyList.append(float(finalCalc))
                     break
-                elif math == "-":  
+                elif numIn == "-":  
                     for i in range(len(nums[1:])):
                         finalCalc = finalCalc - nums[i+1]
+                    print("Answer: ", finalCalc)
+                    historyList.append(float(finalCalc))
+                    break
+                elif numIn == "^2":
+                    print("What power would you like to raise to?")
+                    power = float(input(""))
+                    finalCalc = nums[0]
+                    finalCalc = finalCalc ** power
+                    print("Answer: ", finalCalc)
+                    historyList.append(float(finalCalc))
+                    break
+                elif numIn.lower() == "square root" or numIn.lower() == "sqrt" or numIn.lower() == "sqr rt" or numIn.lower() == "sqr root":
+                    finalCalc = math.sqrt(nums[0])
                     print("Answer: ", finalCalc)
                     historyList.append(float(finalCalc))
                     break
